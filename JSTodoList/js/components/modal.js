4acff94c24e20d277dae904000fbf533fb66efa1
LuisACG7 <cruz.guerrero.luis1jvesp@gmail.com>
2024-11-07 10:27:07 -0600

    Modal

--- JSTodoList/js/components/modal.js
+++ JSTodoList/js/components/modal.js
@@ -4,10 +4,10 @@ export default class Modal {
   constructor() {
     this.title = document.getElementById('modal-title');
     this.description = document.getElementById('modal-description');
-    this.btn = document.getElementById('modal-btn');
     this.completed = document.getElementById('modal-completed');
+    this.difficulty = document.getElementById('modal-difficulty'); // Campo de dificultad en el modal
+    this.btn = document.getElementById('modal-btn');
     this.alert = new Alert('modal-alert');
-
     this.todo = null;
   }
 
@@ -16,6 +16,7 @@ export default class Modal {
     this.title.value = todo.title;
     this.description.value = todo.description;
     this.completed.checked = todo.completed;
+    this.difficulty.value = todo.difficulty; // Configuramos el valor de dificultad
   }
 
   onClick(callback) {
@@ -27,11 +28,13 @@ export default class Modal {
 
       $('#modal').modal('toggle');
 
+      // Pasamos la dificultad junto con los otros valores al callback
       callback(this.todo.id, {
         title: this.title.value,
         description: this.description.value,
         completed: this.completed.checked,
+        difficulty: this.difficulty.value
       });
-    }
+    };
   }
 }
